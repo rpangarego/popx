1e87db98132d5e0db246114c52a9a9f70a5f22da
rpangarego <rpangarego@gmail.com>
2021-03-26 18:34:37 +0700

    update db

--- db/popx.sql
+++ db/popx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 23, 2021 at 12:55 PM
+-- Generation Time: Mar 26, 2021 at 12:33 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -39,7 +39,11 @@ CREATE TABLE `foods` (
 --
 
 INSERT INTO `foods` (`id`, `menu`, `category`, `price`) VALUES
-(1, 'French Fries', 'Food', 12000);
+(1, 'French Fries', 'Food', 12000),
+(2, 'Vanilla Milk Boba', 'Drink', 8000),
+(3, 'Curry Ramen', 'Food', 25000),
+(7, 'Kimchi', 'Food', 10000),
+(9, 'Something', 'Drink', 99000);
 
 -- --------------------------------------------------------
 
@@ -60,7 +64,8 @@ CREATE TABLE `majors` (
 INSERT INTO `majors` (`id`, `major_code`, `major`) VALUES
 (1, 'RBT', 'Robotics'),
 (2, 'ACC', 'Accounting'),
-(3, 'MTC', 'Mathematics');
+(3, 'MTC', 'Mathematics'),
+(4, 'ART', 'Statue Art');
 
 -- --------------------------------------------------------
 
@@ -81,7 +86,8 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `description`, `writer`, `created_at`) VALUES
-(1, 'hello again', 'Testing input', 'Popcorn', '2021-03-23 18:38:24');
+(2, 'How to create frontend with javascript?', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea numquam repellat placeat deleniti, odio fugit, doloremque ut mollitia nulla sed commodi minus. Autem eos, expedita beatae iste atque rem voluptatem.', 'Sushi', '2021-03-25 11:16:12'),
+(5, 'How to make a good ramen', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Ramen', '2021-03-26 08:45:39');
 
 -- --------------------------------------------------------
 
@@ -107,7 +113,32 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `full_name`, `birth_date`, `birth_place`, `gender`, `city`, `major_id`, `food_id`, `image_url`, `created_at`) VALUES
-(1, 'Popcorn', '2021-03-09 00:00:00', 'Seoul', '1', 'London', 1, 1, '', '2021-03-23 18:49:56');
+(1, 'Popcorn', '2021-03-24 00:00:00', 'Seoul', '1', 'London', 1, 1, '', '2021-03-23 18:49:56'),
+(2, 'Park Seo Joon', '2020-11-26 00:00:00', 'Busan', '1', 'Busan', 3, 1, '', '2021-03-24 09:49:24'),
+(3, 'Kim Se Jeong', '2021-03-23 00:00:00', 'Seoul', '2', 'Seoul', 2, 1, '', '2021-03-24 09:49:55'),
+(5, 'Jeon Mi Do', '2021-03-02 00:00:00', 'Tokyo', '2', 'somewhere', 3, 7, '', '2021-03-26 18:13:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `status` enum('1','2','3') NOT NULL COMMENT '1=admin, 2=headmaster, 3=students'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `status`) VALUES
+(1, 'popcorn', '123', '1'),
+(2, 'headmaster', '123', '2'),
+(3, 'student', '123', '3');
 
 --
 -- Indexes for dumped tables
@@ -138,6 +169,12 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -145,25 +182,31 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `foods`
 --
 ALTER TABLE `foods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `majors`
 --
 ALTER TABLE `majors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
